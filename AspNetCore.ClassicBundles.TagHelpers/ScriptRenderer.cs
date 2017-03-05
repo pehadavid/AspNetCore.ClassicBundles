26493476f225abb307dba6584ca5d0a374cb8458
Pierre-Alain David <pa@nextinpact.com>
2017-03-05 19:35:37 +0100

    ViewComponents

---
+++ AspNetCore.ClassicBundles.TagHelpers/ScriptRenderer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AspNetCore.ClassicBundles.TagHelpers
+{
+    [ViewComponent(Name = "ScriptRenderer")]
+    public class ScriptRenderer : ViewComponent
+    {       
+        public async Task<IViewComponentResult> InvokeAsync(string bundlePath)
+        {
+           
+            var bundle = BundleCollection.Instance.GetBundle(bundlePath);
+            if (BundleCollection.Instance.IsMinMode)
+            {
+                return new HtmlContentViewComponentResult(bundle.MinRender());
+            }
+            return new HtmlContentViewComponentResult(bundle.FlatRender());
+        }
+
+    }
+}
---
+++ AspNetCore.ClassicBundles.TagHelpers/StyleRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace AspNetCore.ClassicBundles.TagHelpers
+{
+    [ViewComponent(Name = "StyleRenderer")]
+    public class StyleRenderer : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(string bundlePath)
+        {
+
+            var bundle = BundleCollection.Instance.GetBundle(bundlePath);
+            if (BundleCollection.Instance.IsMinMode)
+            {
+                return new HtmlContentViewComponentResult(bundle.MinRender());
+            }
+            return new HtmlContentViewComponentResult(bundle.FlatRender());
+        }
+
+    }
+}
