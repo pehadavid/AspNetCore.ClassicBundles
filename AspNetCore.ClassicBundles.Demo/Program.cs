d77a23a92235d01b512e98b6e74e8bf7198b5648
Pierre-Alain David <pa@pcinpact.com>
2021-01-06 07:30:51 +0100

    Upgrade demo project to ASP.Net Core 5.x

--- AspNetCore.ClassicBundles.Demo/Program.cs
+++ AspNetCore.ClassicBundles.Demo/Program.cs
@@ -16,7 +16,6 @@ namespace AspNetCore.ClassicBundles.Demo
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
